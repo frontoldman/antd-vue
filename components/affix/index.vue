3c77b6c7a822e5a319facef8a20620597c89e694
zhangran <529130510@qq.com>
2016-05-13 19:41:16 +0800

    修改目录名称

--- components/affix/index.vue
+++ components/affix/index.vue
@@ -2,26 +2,21 @@
  * Created by randada on 2016/5/13.
  */
  
- <template>
-    <div>
-        <h1>I am a Counter Component. Edit me in dev mode.</h1>
-        <p>Current count: {{count}}</p>
-    </div>
- </template>
- 
+
  <script>
 
     export default {
       data () {
-        return { count: 0 }
+        return {
+            offsetTop: null,
+            offsetBottom: null
+        }
       },
       ready () {
-        this.handle = setInterval(() => {
-          this.count++
-        }, 1000)
+
       },
       destroyed () {
-        clearInterval(this.handle)
+
       }
     }
 
