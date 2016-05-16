b88069410c91e7605bcc897384fe0f47525c87bf
luck_zr <529130510@qq.com>
2016-05-16 14:29:37 +0800

    add affix

--- components/affix/index.vue
+++ components/affix/index.vue
@@ -9,43 +9,91 @@
 </style>
 
 <template>
-    <div class="affix" v-bind:style="affixStyle">
+    <div :class="hasFixedClass ? 'affix' : ''" :style="affixStyle">
         <slot></slot>
     </div>
 </template>
 
 <script>
 
-    import { isDefined } from '../../utils/'
+    import {isDefined} from '../../utils/'
 
     export default {
-        props:[
+        props: [
             'offsetTop',
             'offsetBottom'
         ],
+        data() {
+            return {
+                initPosition: null,
+                hasFixedClass: false,
+                top: null,
+                bottom: null
+            }
+        },
         ready () {
-
+            this.getInitPosition();
+            window.addEventListener('scroll', this._handleScroll, false);
         },
         computed: {
             affixStyle() {
-
-                var top, bottom;
-                if(isDefined(this.offsetTop)){
-                    top = this.offsetTop + 'px';
-                }else if(isDefined(this.offsetBottom)){
-                    bottom = this.offsetBottom + 'px';
-                }else{
-                    top = 0;
-                }
-
                 return {
-                    top,
-                    bottom
+                    top: this.top,
+                    bottom: this.bottom
                 }
+            },
+            _offsetTop() {
+                return this.offsetTop * 1;
+            },
+            _offsetBottom() {
+                return this.offsetBottom * 1;
             }
         },
         destroyed () {
 
+        },
+        methods: {
+            _handleScroll() {
+                this.placeEle();
+            },
+            placeEle() {
+                var top, bottom, hasFixedClass = false;
+                var scrollTop = window.scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
+                var docHeight = document.body.clientHeight;
+
+                if (isDefined(this.offsetTop)) {
+                    if (this.initPosition.top <= scrollTop + this._offsetTop) {
+                        top = this._offsetTop + 'px';
+                        hasFixedClass = true;
+                    }
+                } else if (isDefined(this.offsetBottom)) {
+                    if(this.initPosition.bottom >= (docHeight - scrollTop) + this._offsetBottom){
+
+                    }
+
+                    bottom = this._offsetBottom + 'px';
+                    hasFixedClass = true;
+                } else {
+                    if (this.initPosition.top <= scrollTop) {
+                        top = 0;
+                        hasFixedClass = true;
+                    }
+                }
+
+                this.top = top;
+                this.bottom = bottom;
+                this.hasFixedClass = hasFixedClass;
+
+            },
+            getInitPosition() {
+                //当元素位于视窗外面的时候,位置信息是负数,不影响计算
+                this.initPosition = this.$el.getBoundingClientRect();
+            },
+            getScrollTop() {
+
+
+
+            }
         }
     }
 
