b5068f58fdfba593c7e06071841a4cd0bf7c2cd6
luck_zr <529130510@qq.com>
2016-05-17 07:01:40 +0800

    add button

--- components/index.js
+++ components/index.js
@@ -2,12 +2,14 @@
  * Created by 52913 on 2016/5/13.
  */
 import Vue from 'vue'
-import affix from './affix/index.vue'
+import * as Affix from './Affix/index.vue'
+import * as Button from './Button/index.vue'
 
 new Vue({
     el: 'body',
     components: {
-        affix: affix
+        Affix,
+        Button
     }
 })
 
