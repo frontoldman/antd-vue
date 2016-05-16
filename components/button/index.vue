96bfe5ff5d45ffc63048a711944a232092abff3f
luck_zr <529130510@qq.com>
2016-05-16 20:49:03 +0800

    add button

---
+++ components/button/index.vue
@@ -0,0 +1,33 @@
+/**
+* Created by zhangran on 16/5/16.
+*/
+
+<style type="text/style">
+
+</style>
+
+<template>
+
+</template>
+
+<script>
+
+    export default {
+        props:{
+            type: {
+                type: String,
+                default: 'primary'
+            },
+            htmlType: {
+                type: String,
+                default: 'button'
+            },
+            size: {
+                type: String,
+                default: 'default'
+            }
+
+        }
+    }
+
+</script>
