6c98fbf38a6abd996b9a2241608a4ab67009471c
luck_zr <529130510@qq.com>
2016-05-18 14:23:32 +0800

    add alert

---
+++ components/Alert/Alert.vue
@@ -0,0 +1,67 @@
+/**
+* Created by zhangran on 16/5/18.
+*/
+
+<style type="text/style">
+
+</style>
+
+<template>
+    <div :class="alertClasses">
+        <i v-show="showIcon" :class="iconClasses"></i>
+        <span class="ant-alert-message">{{message}}</span>
+        <span class="ant-alert-description" v-if="description">{{description}}</span>
+        <a class="ant-alert-close-icon" @click="close" v-if="closable"><i class=" anticon anticon-cross"></i></a>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            message: String,
+            type: String,
+            description: String,
+            closable: Boolean,
+            onClose: Function,
+            showIcon: Boolean
+        },
+        computed: {
+            alertClasses() {
+                return {
+                    [`ant-alert`]: 1,
+                    [`ant-alert-${this.type}`]: this.type,
+                    [`ant-alert-with-description`]: this.description,
+                    [`ant-alert-no-icon`]: !this.showIcon
+                }
+            },
+            iconClasses() {
+                const description = this.description;
+                const iconStyles = {
+                    success: !description ? 'anticon-check-circle' : 'anticon-check-circle-o',
+                    info: !description ? 'anticon-info-circle' : 'anticon-info-circle-o',
+                    warning: !description ? 'anticon-exclamation-circle' : 'anticon-exclamation-circle-o',
+                    error: !description ? 'anticon-cross-circle' : 'anticon-cross-circle-o'
+                }
+
+                console.log(this.showIcon)
+
+                return {
+                    ['anticon']: 1,
+                    ['ant-alert-icon']: 1,
+                    [iconStyles[this.type]]: this.showIcon
+                }
+            }
+        },
+        ready() {
+
+        },
+        methods: {
+            close() {
+                //this.show = false;
+                this.onClose && this.onClose();
+            }
+        }
+    }
+
+</script>
--- components/index.js
+++ components/index.js
@@ -2,32 +2,35 @@
  * Created by 52913 on 2016/5/13.
  */
 // import 'reset.css'
-import 'antd/dist/antd.css'
-import Vue from 'vue'
-import * as Affix from './Affix/Affix.vue'
-import * as Button from './Button/Button.vue'
-import * as ButtonGroup from './Button/ButtonGroup.vue'
-import * as Icon from './Icon/Icon.vue'
-import * as Row from './Layout/Row.vue'
-import * as Column from './Layout/Column.vue'
+import "antd/dist/antd.css";
+import Vue from "vue";
+import * as Affix from "./Affix/Affix.vue";
+import * as Button from "./Button/Button.vue";
+import * as ButtonGroup from "./Button/ButtonGroup.vue";
+import * as Icon from "./Icon/Icon.vue";
+import * as Row from "./Layout/Row.vue";
+import * as Column from "./Layout/Column.vue";
+import * as Alert from "./Alert/Alert.vue";
 
 new Vue({
-    el: 'body',
-    components: {
-        Affix,
-        Button,
-        ButtonGroup,
-        Icon,
-        Row,
-        Column
-    },
-    data:{
-      loading: false
-    },
-    methods:{
-        testClick() {
-            console.log('click happened!')
-        }
+  el: 'body',
+  components: {
+    Affix,
+    Button,
+    ButtonGroup,
+    Icon,
+    Row,
+    Column,
+    Alert
+  },
+  data: {
+    loading: false,
+    alertShow: false
+  },
+  methods: {
+    testClick() {
+      console.log('click happened!')
     }
+  }
 })
 
