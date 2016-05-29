15fade787d778cb0057473655203fb115c72ddb2
luck_zr <529130510@qq.com>
2016-05-29 21:35:16 +0800

    badge 换个行样式会出问题

--- components/Badge/Badge.vue
+++ components/Badge/Badge.vue
@@ -8,44 +8,12 @@
 
 <template>
     <span class="ant-badge" title="5">
-        <slot></slot>
-        <sup data-show="true" class="ant-scroll-number ant-badge-count" height="18">
-            <span class="ant-scroll-number-only" style="transition: none; transform: translate3d(0px, -270px, 0px); height: 18px;">
-                <p>0</p>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-                <p>9</p>
-
-                <p>0</p>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p class="current">5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-                <p>9</p>
-
-                <p>0</p>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-                <p>9</p>
-            </span>
-        </sup>
-    </span>
+        <a href="#" class="head-example"></a><sup data-show="true"
+                                                                                class="ant-scroll-number ant-badge-count"
+                                                                                height="18"><span
+            class="ant-scroll-number-only"
+            style="transition: none; transform: translate3d(0px, -270px, 0px); height: 18px;"><p>0</p><p>1</p><p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p><p>8</p><p>9</p><p>0</p><p>1</p><p>2</p><p>3</p><p>4</p><p
+            class="current">5</p><p>6</p><p>7</p><p>8</p><p>9</p><p>0</p><p>1</p><p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p><p>8</p><p>9</p></span></sup></span>
 </template>
 
 <script>
