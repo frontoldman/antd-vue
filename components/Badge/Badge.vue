1c8a119e53f6a921910ca5813ea574563141b8f3
luck_zr <529130510@qq.com>
2016-05-30 09:36:36 +0800

    v-else 确定管用吗

--- components/Badge/Badge.vue
+++ components/Badge/Badge.vue
@@ -8,13 +8,21 @@
 
 <template>
     <span class="ant-badge" title="{{count}}">
-        <slot></slot><sup data-show="true" class="ant-scroll-number ant-badge-count" height="18">
-        <span :style="{transform: 'translate3d(0px, ' + (-18*(count*1+9)) + 'px, 0px'}" class="ant-scroll-number-only"
-            style="transition: none;  height: 18px;">
+        <slot></slot><sup
+            data-show="true"
+            class="ant-scroll-number ant-badge-count" height="{{height}}"
+            :style="supStyle"
+        >
+        <span v-if="count*1<overflowCount*1" v-for="itemCount in countAry" track-by="$index"
+              :style="{transform: 'translate3d(0px, ' + (-height*(itemCount*1+9)) + 'px, 0px', height: height + 'px'}"
+              class="ant-scroll-number-only"
+            style="transition: none; ">
             <p v-for="n in 9">{{n}}</p>
             <p v-for="n in 9" :class="{'current': count == n}">{{n}}</p>
             <p v-for="n in 9">{{n}}</p>
         </span>
+        <span v-if="count*1>=overflowCount*1">{{(overflowCount*1-1) + '+'}}</span>
+
     </sup>
     </span>
 </template>
@@ -23,15 +31,33 @@
 
     export default {
         props: {
-            count: [String, Number]
+            count: [String, Number],
+            height: {
+                type: [String, Number],
+                default: 18
+            },
+            badgeStyle: {
+                type: [Object, String]
+            },
+            overflowCount: {
+                type: [String, Number],
+                default: 100
+            }
         },
         data() {
             return {
-                //data here
+                countAry: [],
+                supStyle: {}
             }
         },
         ready() {
-            console.log(this.count)
+            this.countAry = (this.count + '').split('');
+
+            if(typeof this.style === 'String'){
+                this.supStyle = JSON.parse(this.badgeStyle);
+            }else{
+                this.supStyle = this.badgeStyle;
+            }
         },
         methods: {
             
